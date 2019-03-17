985267a454f5e68dd3978bf59cb4696433ef4deb
Dominik Krawczyk <sxsilder@gmail.com>
2019-03-17 19:01:03 +0100

    rebuild gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,20 +5,19 @@ function createNode(element) {
 function append(parent, el) {
 	return parent.appendChild(el);
 }
+const apiUrl = 'https://pixabay.com/api/?key=10345617-64d66bf9e1b081a57af727dca&q=beauty&girl&image_type=photo&pretty=true';
 
-const gallery = document.querySelector("#images");
-const apiUrl = "https://pixabay.com/api/?key=10345617-64d66bf9e1b081a57af727dca&q=beauty&girl&image_type=photo&pretty=true";
-fetch(apiUrl)
-	.then(res => res.json())
-	.then(function (data) {
-		let images = data.hits;
-		return images.map(function (image) {
-			let img = createNode('img');
-			img.src = image.largeImageURL;
-			append(gallery, img);
-		})
-	})
+async function galleryFetch() {
+	const imageResponse = await fetch(apiUrl);
+	const galleryPic = await imageResponse.json();
+	const gallery = document.querySelector("#images");
+	for(let i = 0, len = 15; i < len; i++) {
+		let img = createNode('img');
+		img.src = galleryPic.hits[i].largeImageURL;
+		append(gallery, img);
+	}
+	console.log(galleryPic.hits[0].largeImageURL);
+}
 
-	.catch(function (error) {
-		console.log(error);
-	});
+
+galleryFetch();
