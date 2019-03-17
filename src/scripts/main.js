4f9a8146c88df81a925a68e3ecfd9e4824b1cc8a
Dominik Krawczyk <sxsilder@gmail.com>
2019-03-17 19:18:47 +0100

    js change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,15 @@
-function createNode(element) {
-	return document.createElement(element);
-}
-
-function append(parent, el) {
-	return parent.appendChild(el);
-}
 const apiUrl = 'https://pixabay.com/api/?key=10345617-64d66bf9e1b081a57af727dca&q=beauty&girl&image_type=photo&pretty=true';
 
 async function galleryFetch() {
-	const imageResponse = await fetch(apiUrl);
-	const galleryPic = await imageResponse.json();
-	const gallery = document.querySelector("#images");
-	for(let i = 0, len = 15; i < len; i++) {
-		let img = createNode('img');
-		img.src = galleryPic.hits[i].largeImageURL;
-		append(gallery, img);
-	}
-	console.log(galleryPic.hits[0].largeImageURL);
-}
+    const imageResponse = await fetch(apiUrl);
+    const galleryPic = await imageResponse.json();
+    const galleryEl = document.querySelector("#images");
 
+    const hits = galleryPic.hits.slice(0, 15);
+
+    for (const hit of hits) {
+        galleryEl.insertAdjacentHTML('beforeend', `<img src="${hit.largeImageURL}">`);
+    }
+}
 
 galleryFetch();
